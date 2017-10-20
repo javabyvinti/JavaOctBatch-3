e9e94ec368a147dac49b6e7d6f43c0f98568bd9f
javabyvinti <32234213+javabyvinti@users.noreply.github.com>
2017-10-20 14:51:20 +0530

    Add files via upload

---
+++ 17 Oct- Java Data/newfile.java
@@ -0,0 +1,57 @@
+//hello world
+
+// OOPS --> class and object
+
+
+class hello{
+
+	//main method/ function
+	
+	//public --> package centric language
+	//static --> no need to create object for calling 	// main , first loaded into memory
+	// void --> no return type
+	// main --> name of method or function
+
+	// String--> Predefined class
+
+//	public static void main(String args[])
+//	{
+
+		void data(){
+
+		System.out.println("Hello in the world");
+		}
+		//where to  print --> console window
+		//out	--> object --> PrintStream 
+//}
+
+	// int args[] = {4,7,3,7,3,4}-->
+	
+	// String args[] = {"hello hi im in java", "data rejkre", "trtr", "9854544545"} --> array
+// 54 + 87 -->5487 
+}
+
+
+
+
+	// int value = 10;
+	// int value = 10;
+	// Integer value = 10;//reference type variable
+	
+
+	//Integer value = new Integer();
+
+	//float --> Float , double--> Double
+
+
+
+
+
+
+
+
+
+
+
+
+
